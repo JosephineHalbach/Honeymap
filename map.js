fe5c8d27b31dacceb0087e3221b92ad0d9b7ae14
Josephine Halbach <168281515+JosephineHalbach@users.noreply.github.com>
2025-07-03 15:09:01 +0200

    Update map.js

--- map.js
+++ map.js
@@ -3,28 +3,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap',
 }).addTo(map);
 
-const attackIcon = L.icon({
-  iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Warning_icon.svg',
-  iconSize: [24, 24]
-});
-
-fetch('data.json')
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(entry => {
-      const marker = L.marker([entry.lat, entry.lon], { icon: attackIcon }).addTo(map);
-      marker.bindPopup(
-        `<b>🔴 Angriff</b><br>IP: ${entry.ip}<br>Zeit: ${entry.time}`
-      );
-    });
-  })
-  .catch(err => console.error('Fehler beim Laden der Daten:', err));
-
-const map = L.map('map').setView([20, 0], 2);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap',
-}).addTo(map);
-
 const berlin = [52.52, 13.405]; // Zielstadt für Angriffe
 const attackIcon = L.icon({
   iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Warning_icon.svg',
